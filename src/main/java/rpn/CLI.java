8f4307a05686d6c152751aff6afc85d87a2d76d3
Olivier <Olivier@OLIVIER-PC>
2018-03-08 14:46:52 +0100

    First implementation of the RPN system. Main program and tests should be completed now.

--- src/main/java/rpn/CLI.java
+++ src/main/java/rpn/CLI.java
@@ -1,18 +1,84 @@
 package rpn;
 
+import java.math.BigDecimal;
+import java.util.Scanner;
+import java.util.Stack;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class CLI {
-    public static final void main(String[] args) {
-        String expression = Stream.of(args).collect(Collectors.joining(" "));
+    public static void main(String[] args) {
+        String expression = Stream.of(args).collect(Collectors.joining(" ")).trim();
 
         System.out.println("About to evaluate '" + expression + "'");
-        long result = evaluate(expression);
+
+        BigDecimal result = !expression.equals("") ? BigDecimal.valueOf(evaluate(expression)) : BigDecimal.valueOf(0.0);
+
         System.out.println("> " + result);
     }
 
-    static long evaluate(String expression) {
-        return 0;
+    static Double evaluate(String expression) {
+        Scanner scanner = new Scanner(expression);
+        Stack<Double> stack = new Stack<>();
+        String occurrence;
+
+        while(scanner.hasNext()){
+            occurrence = scanner.next();
+
+            if(!isOperator(occurrence)) {
+                try {
+                    stack.push(Double.parseDouble(occurrence));
+                } catch (NumberFormatException arrEx) {
+                    throw new NumberFormatException("Bad formatted RPN.\n");
+                }
+            }
+            else {
+                if(stack.size() <= 1){
+                    System.out.println("Invalid RPN. Check the last characters of your RPN expression.");
+                    return 0.0;
+                }
+                else {
+                    compute(stack, occurrence);
+                    if(stack.size() == 0) return 0.0;
+                }
+            }
+        }
+
+        if(stack.size() > 1) {
+            System.out.println("Lack of mathematical operators to evaluate the expression.");
+            return 0.0;
+        }
+
+        return stack.pop();
+    }
+
+    private static void compute(Stack<Double> stack, String occurrence){
+        switch (occurrence) {
+            case "+":
+                stack.push(stack.pop() + stack.pop());
+                break;
+            case "-":
+                stack.push(-stack.pop() + stack.pop());
+                break;
+            case "*":
+                stack.push(stack.pop() * stack.pop());
+                break;
+            case "/":
+                Double first = stack.pop();
+                Double second = stack.pop();
+
+                if(first == 0){
+                    System.out.println("Division error. Trying to divide by zero?");
+                    return;
+                }
+
+                stack.push(second / first);
+
+                break;
+        }
+    }
+
+    private static boolean isOperator(String str){
+        return str.equals("+") || str.equals("-") || str.equals("*") || str.equals("/");
     }
 }
